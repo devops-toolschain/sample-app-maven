4212749446f3bc210fb3394fae9cd96ee50672a2
vijaykumar-r <vijaykumar-r@hotmail.com>
2022-10-23 15:25:19 +0100

    Test

--- java-app/src/main/java/com/app/example/App.java
+++ java-app/src/main/java/com/app/example/App.java
@@ -20,6 +20,6 @@ public class App extends SpringBootServletInitializer {
 
    @RequestMapping(value = "/")
    public String hello() {
-      return "<center>Hello Publicis Sapient - Welcome !!!</center>";
+      return "<center>Hello Publicis Sapient !!!</center>";
    }
 }
