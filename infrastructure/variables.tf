6929f410dc1121281c8af6b36a3844b8fec8779a
vijaykumar-r <vijaykumar-r@hotmail.com>
2022-10-25 22:20:44 +0100

    Delete pub resources

--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -1,135 +1,135 @@
-locals {
+# locals {
 
-  vm_name = "web-vm"
-  ansible_vm_name = "ansible_vm"
-  vm_size = "Standard_D2s_v3"
+#   vm_name = "web-vm"
+#   ansible_vm_name = "ansible_vm"
+#   vm_size = "Standard_D2s_v3"
 
-  web_nsg_rules = {
-    allow_ssh = {
-      name                       = "allow-ssh"
-      priority                   = "100"
-      direction                  = "Inbound"
-      access                     = "Allow"
-      protocol                   = "Tcp"
-      source_address_prefix      = "${chomp(data.http.myip.response_body)}"
-      source_port_range          = "*"
-      destination_address_prefix = "*"
-      destination_port_range     = "22"
-      description                = "Allow port 22"
-    }
+#   web_nsg_rules = {
+#     allow_ssh = {
+#       name                       = "allow-ssh"
+#       priority                   = "100"
+#       direction                  = "Inbound"
+#       access                     = "Allow"
+#       protocol                   = "Tcp"
+#       source_address_prefix      = "${chomp(data.http.myip.response_body)}"
+#       source_port_range          = "*"
+#       destination_address_prefix = "*"
+#       destination_port_range     = "22"
+#       description                = "Allow port 22"
+#     }
 
-    allow_80 = {
-      name                       = "allow-80"
-      priority                   = "110"
-      direction                  = "Inbound"
-      access                     = "Allow"
-      protocol                   = "Tcp"
-      source_address_prefix      = "*"
-      source_port_range          = "*"
-      destination_address_prefix = "*"
-      destination_port_range     = "80"
-      description                = "Allow port 80"
-    }
+#     allow_80 = {
+#       name                       = "allow-80"
+#       priority                   = "110"
+#       direction                  = "Inbound"
+#       access                     = "Allow"
+#       protocol                   = "Tcp"
+#       source_address_prefix      = "*"
+#       source_port_range          = "*"
+#       destination_address_prefix = "*"
+#       destination_port_range     = "80"
+#       description                = "Allow port 80"
+#     }
 
-    allow_443 = {
-      name                       = "allow-443"
-      priority                   = "120"
-      direction                  = "Inbound"
-      access                     = "Allow"
-      protocol                   = "Tcp"
-      source_address_prefix      = "*"
-      source_port_range          = "*"
-      destination_address_prefix = "*"
-      destination_port_range     = "443"
-      description                = "Allow port 443"
-    }
+#     allow_443 = {
+#       name                       = "allow-443"
+#       priority                   = "120"
+#       direction                  = "Inbound"
+#       access                     = "Allow"
+#       protocol                   = "Tcp"
+#       source_address_prefix      = "*"
+#       source_port_range          = "*"
+#       destination_address_prefix = "*"
+#       destination_port_range     = "443"
+#       description                = "Allow port 443"
+#     }
 
-    allow_8080 = {
-      name                       = "allow-8080"
-      priority                   = "130"
-      direction                  = "Inbound"
-      access                     = "Allow"
-      protocol                   = "Tcp"
-      source_address_prefix      = "*"
-      source_port_range          = "*"
-      destination_address_prefix = "*"
-      destination_port_range     = "8080"
-      description                = "Allow port 8080"
-    }
-  }
+#     allow_8080 = {
+#       name                       = "allow-8080"
+#       priority                   = "130"
+#       direction                  = "Inbound"
+#       access                     = "Allow"
+#       protocol                   = "Tcp"
+#       source_address_prefix      = "*"
+#       source_port_range          = "*"
+#       destination_address_prefix = "*"
+#       destination_port_range     = "8080"
+#       description                = "Allow port 8080"
+#     }
+#   }
 
-  # ansible_nsg_rules = {
-  #   allow_ssh = {
-  #     name                       = "allow-ssh"
-  #     priority                   = "100"
-  #     direction                  = "Inbound"
-  #     access                     = "Allow"
-  #     protocol                   = "Tcp"
-  #     source_address_prefix      = "*"
-  #     source_port_range          = "*"
-  #     destination_address_prefix = "*"
-  #     destination_port_range     = "22"
-  #     description                = "Allow port 22"
-  #   }
-  # }
-}
+#   # ansible_nsg_rules = {
+#   #   allow_ssh = {
+#   #     name                       = "allow-ssh"
+#   #     priority                   = "100"
+#   #     direction                  = "Inbound"
+#   #     access                     = "Allow"
+#   #     protocol                   = "Tcp"
+#   #     source_address_prefix      = "*"
+#   #     source_port_range          = "*"
+#   #     destination_address_prefix = "*"
+#   #     destination_port_range     = "22"
+#   #     description                = "Allow port 22"
+#   #   }
+#   # }
+# }
 
-variable "os_disk" {
-  type = object({
-    caching              = string
-    storage_account_type = string
-    disk_size_gb         = string
-  })
-  default = {
-    caching              = "ReadOnly"
-    storage_account_type = "StandardSSD_LRS"
-    disk_size_gb         = "30"
-  }
-}
+# variable "os_disk" {
+#   type = object({
+#     caching              = string
+#     storage_account_type = string
+#     disk_size_gb         = string
+#   })
+#   default = {
+#     caching              = "ReadOnly"
+#     storage_account_type = "StandardSSD_LRS"
+#     disk_size_gb         = "30"
+#   }
+# }
 
-variable "source_image_reference" {
-  type = object({
-    publisher = string
-    offer     = string
-    sku       = string
-    version   = string
-  })
-  default = {
-    publisher = "Canonical"
-    offer     = "UbuntuServer"
-    sku       = "18.04-LTS"
-    version   = "latest"
-  }
-}
+# variable "source_image_reference" {
+#   type = object({
+#     publisher = string
+#     offer     = string
+#     sku       = string
+#     version   = string
+#   })
+#   default = {
+#     publisher = "Canonical"
+#     offer     = "UbuntuServer"
+#     sku       = "18.04-LTS"
+#     version   = "latest"
+#   }
+# }
 
-variable "location" {
-  type    = string
-  default = "UK South"
-}
+# variable "location" {
+#   type    = string
+#   default = "UK South"
+# }
 
-variable "env" {
-  type = string
-}
+# variable "env" {
+#   type = string
+# }
 
-variable "tags" {
-  type = map(string)
-  default = {
-    project = "Publicis"
-  }
-}
-
-variable "web_vm_tags" {
-  type = map(string)
-  default = {
-    project = "Publicis"
-    group = "web"
-  }
-}
-
-# variable "ansible_vm_tags" {
+# variable "tags" {
 #   type = map(string)
 #   default = {
 #     project = "Publicis"
-#     group = "ansible"
 #   }
 # }
+
+# variable "web_vm_tags" {
+#   type = map(string)
+#   default = {
+#     project = "Publicis"
+#     group = "web"
+#   }
+# }
+
+# # variable "ansible_vm_tags" {
+# #   type = map(string)
+# #   default = {
+# #     project = "Publicis"
+# #     group = "ansible"
+# #   }
+# # }
--- infrastructure/web_vm.tf
+++ infrastructure/web_vm.tf
@@ -1,105 +1,105 @@
-data "http" "myip" {
-  url = "http://ipv4.icanhazip.com"
-}
+# data "http" "myip" {
+#   url = "http://ipv4.icanhazip.com"
+# }
 
-module "web_nsg" {
-  source               = "github.com/devops-toolschain/azure-terraform-modules.git//az-nsg-vm"
-  resource_group_name  = module.pub_rg.name
-  location             = var.location
-  virtual_machine_name = local.vm_name
-  tags                 = var.tags
-}
+# module "web_nsg" {
+#   source               = "github.com/devops-toolschain/azure-terraform-modules.git//az-nsg-vm"
+#   resource_group_name  = module.pub_rg.name
+#   location             = var.location
+#   virtual_machine_name = local.vm_name
+#   tags                 = var.tags
+# }
 
-module "web_nsg_rules" {
-  source                      = "github.com/devops-toolschain/azure-terraform-modules.git//az-nsg-security-rule"
-  resource_group_name         = module.pub_rg.name
-  network_security_group_name = module.web_nsg.name
-  nsg_rules_prefix_range      = local.web_nsg_rules
-}
+# module "web_nsg_rules" {
+#   source                      = "github.com/devops-toolschain/azure-terraform-modules.git//az-nsg-security-rule"
+#   resource_group_name         = module.pub_rg.name
+#   network_security_group_name = module.web_nsg.name
+#   nsg_rules_prefix_range      = local.web_nsg_rules
+# }
 
-module "pub_publicip" {
-    source                = "github.com/devops-toolschain/azure-terraform-modules.git//az-publicip"
-    publicip_label        = local.vm_name
-    resource_group_name   = module.pub_rg.name
-    location              = var.location
-    sku                   = "Standard"
-    allocation_method     = "Static"
-    tags                  = var.web_vm_tags
-}
+# module "pub_publicip" {
+#     source                = "github.com/devops-toolschain/azure-terraform-modules.git//az-publicip"
+#     publicip_label        = local.vm_name
+#     resource_group_name   = module.pub_rg.name
+#     location              = var.location
+#     sku                   = "Standard"
+#     allocation_method     = "Static"
+#     tags                  = var.web_vm_tags
+# }
 
-module "web_vm_nic" {
-  source                        = "github.com/devops-toolschain/azure-terraform-modules.git//az-vm-nic"
-  virtual_machine_name          = "spring-app-vm"
-  resource_group_name           = module.pub_rg.name
-  location                      = var.location
-  network_security_group_id     = module.web_nsg.id
-  subnet_id                     = module.pub_vnet_subnet.id
-  private_ip_address            = null
-  private_ip_address_allocation = "Dynamic"
-  public_ip_address_id          = module.pub_publicip.id
-  enable_accelerated_networking = "false"
-  tags                          = var.web_vm_tags
-}
+# module "web_vm_nic" {
+#   source                        = "github.com/devops-toolschain/azure-terraform-modules.git//az-vm-nic"
+#   virtual_machine_name          = "spring-app-vm"
+#   resource_group_name           = module.pub_rg.name
+#   location                      = var.location
+#   network_security_group_id     = module.web_nsg.id
+#   subnet_id                     = module.pub_vnet_subnet.id
+#   private_ip_address            = null
+#   private_ip_address_allocation = "Dynamic"
+#   public_ip_address_id          = module.pub_publicip.id
+#   enable_accelerated_networking = "false"
+#   tags                          = var.web_vm_tags
+# }
 
-# # Create Virtual Machine
-module "web_vm" {
-  source = "github.com/devops-toolschain/azure-terraform-modules.git//az-vm-linux"
+# # # Create Virtual Machine
+# module "web_vm" {
+#   source = "github.com/devops-toolschain/azure-terraform-modules.git//az-vm-linux"
 
-  # Common configuration
-  vm_name             = local.vm_name
-  computer_name       = "webvm"
-  resource_group_name = module.pub_rg.name
-  location            = var.location
+#   # Common configuration
+#   vm_name             = local.vm_name
+#   computer_name       = "webvm"
+#   resource_group_name = module.pub_rg.name
+#   location            = var.location
 
-  admin_username = "azureadmin"
-  ssh_public_key = file("ssh_keys/id_pub_project.pub")
+#   admin_username = "azureadmin"
+#   ssh_public_key = file("ssh_keys/id_pub_project.pub")
 
-  # NIC configurtion
-  network_interface_ids = [module.web_vm_nic.id]
+#   # NIC configurtion
+#   network_interface_ids = [module.web_vm_nic.id]
 
-  # VM configuration
-  vm_size                = local.vm_size
-  os_disk                = var.os_disk
-  source_image_reference = var.source_image_reference
-  tags                   = var.web_vm_tags
-}
+#   # VM configuration
+#   vm_size                = local.vm_size
+#   os_disk                = var.os_disk
+#   source_image_reference = var.source_image_reference
+#   tags                   = var.web_vm_tags
+# }
 
-resource "azurerm_log_analytics_workspace" "pub_law" {
-  name                      = "vmloganalytics"
-  resource_group_name       = module.pub_rg.name
-  location                  = var.location
-  sku                       = "PerGB2018"
-  retention_in_days         = 30
-  internet_ingestion_enabled= true
-  internet_query_enabled    = false
-}
+# resource "azurerm_log_analytics_workspace" "pub_law" {
+#   name                      = "vmloganalytics"
+#   resource_group_name       = module.pub_rg.name
+#   location                  = var.location
+#   sku                       = "PerGB2018"
+#   retention_in_days         = 30
+#   internet_ingestion_enabled= true
+#   internet_query_enabled    = false
+# }
 
-resource "azurerm_virtual_machine_extension" "web_vm_monitor" {
-  name                       = "DAExtension"
-  virtual_machine_id         = module.web_vm.vm_id
-  publisher                  = "Microsoft.Azure.Monitoring.DependencyAgent"
-  type                       = "DependencyAgentLinux"
-  type_handler_version       = "9.5"
-  auto_upgrade_minor_version = true
-}
+# resource "azurerm_virtual_machine_extension" "web_vm_monitor" {
+#   name                       = "DAExtension"
+#   virtual_machine_id         = module.web_vm.vm_id
+#   publisher                  = "Microsoft.Azure.Monitoring.DependencyAgent"
+#   type                       = "DependencyAgentLinux"
+#   type_handler_version       = "9.5"
+#   auto_upgrade_minor_version = true
+# }
 
-resource "azurerm_virtual_machine_extension" "web_vm_monitor_oms" {
-  name                 = "OmsAgentForLinux"
-  virtual_machine_id   = module.web_vm.vm_id
-  publisher            = "Microsoft.EnterpriseCloud.Monitoring"
-  type                 = "OmsAgentForLinux"
-  type_handler_version = "1.12"
-  auto_upgrade_minor_version = true
+# resource "azurerm_virtual_machine_extension" "web_vm_monitor_oms" {
+#   name                 = "OmsAgentForLinux"
+#   virtual_machine_id   = module.web_vm.vm_id
+#   publisher            = "Microsoft.EnterpriseCloud.Monitoring"
+#   type                 = "OmsAgentForLinux"
+#   type_handler_version = "1.12"
+#   auto_upgrade_minor_version = true
 
-  settings = <<SETTINGS
-  {
-    "workspaceId": "${azurerm_log_analytics_workspace.pub_law.workspace_id}"
-  }
-  SETTINGS
+#   settings = <<SETTINGS
+#   {
+#     "workspaceId": "${azurerm_log_analytics_workspace.pub_law.workspace_id}"
+#   }
+#   SETTINGS
 
-  protected_settings = <<PROTECTEDSETTINGS
-  {
-    "workspaceKey": "${azurerm_log_analytics_workspace.pub_law.primary_shared_key}"
-  }
-  PROTECTEDSETTINGS
-}
+#   protected_settings = <<PROTECTEDSETTINGS
+#   {
+#     "workspaceKey": "${azurerm_log_analytics_workspace.pub_law.primary_shared_key}"
+#   }
+#   PROTECTEDSETTINGS
+# }
